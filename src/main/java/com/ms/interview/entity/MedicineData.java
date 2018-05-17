feb1b203a960f7efdbb155d574f397bd95a40ad7
Palash Gupta <palash.gupta@sap.com>
2018-05-17 09:23:51 +0530

    Successful setup for medicine data using JSON

---
+++ src/main/java/com/ms/interview/entity/MedicineData.java
@@ -0,0 +1,97 @@
+package com.ms.interview.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity // This tells Hibernate to make a table out of this class
+public class MedicineData {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String genericName;
+    
+    private double price;
+    
+    private String category;
+    
+    private String manufacturer;
+    
+    private Integer quantity;
+    
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd-MM-yyyy")
+    private Date expiryDate;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGenericName() {
+		return genericName;
+	}
+
+	public void setGenericName(String genericName) {
+		this.genericName = genericName;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Date getExpiryDate() {
+		return expiryDate;
+	}
+
+	public void setExpiryDate(Date expiryDate) {
+		this.expiryDate = expiryDate;
+	}
+}
+
--- src/main/java/com/ms/interview/repository/MedicineDataRepository.java
+++ src/main/java/com/ms/interview/repository/MedicineDataRepository.java
@@ -2,11 +2,11 @@ package com.ms.interview.repository;
 
 import org.springframework.data.repository.CrudRepository;
 
-import com.ms.interview.entity.User1;
+import com.ms.interview.entity.MedicineData;
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface UserRepository extends CrudRepository<User1, Long> {
+public interface MedicineDataRepository extends CrudRepository<MedicineData, Long> {
 
 }
